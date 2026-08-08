aaa00e2adae9822003d0f65a89baf2695619fca3
Van <sidorenkov-ia@yandex.ru>
2026-08-08 22:46:35 +0300

    JRTB-3-NEW create command page to telegram bot

---
+++ src/main/java/com/github/javarushcommunity/javarush_telegrambot/command/UnknownCommand.java
@@ -0,0 +1,22 @@
+package com.github.javarushcommunity.javarush_telegrambot.command;
+
+import org.telegram.telegrambots.meta.api.objects.Update;
+import com.github.javarushcommunity.javarush_telegrambot.service.SendBotMessageService;
+
+public class UnknownCommand implements Command {
+
+
+    public final SendBotMessageService sendBotMessageService;
+
+    private final static String UNKNOWN_MESSAGE = "Эта команда мне неизвеста или это не является " +
+            "допустимой командой. /help - список команд";
+
+    public UnknownCommand(SendBotMessageService sendBotMessageService) {
+        this.sendBotMessageService = sendBotMessageService;
+    }
+
+    @Override
+    public void execute(Update update) {
+        sendBotMessageService.sendMessage(update.getMessage().getChatId().toString(), UNKNOWN_MESSAGE);
+    }
+}
