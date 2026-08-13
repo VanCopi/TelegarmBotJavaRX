ce61992437c56b1eae740feeddce5b9bf1dc911f
Van <sidorenkov-ia@yandex.ru>
2026-08-13 23:26:14 +0300

    * Tested deployment process in telegram web version * Added Log.env with BOT_NAME, BOT_TOKEN * Added user file COMMANDS.md with docker's list of commands * Corrected BOT_NAME, BOT_TOKEN positions and uses

--- src/test/java/com/github/javarushcommunity/javarush_telegrambot/JavarushTelegrambotApplicationTests.java
+++ src/test/java/com/github/javarushcommunity/javarush_telegrambot/JavarushTelegrambotApplicationTests.java
@@ -1,10 +1,15 @@
 package com.github.javarushcommunity.javarush_telegrambot;
 
+import com.github.javarushcommunity.javarush_telegrambot.bot.JavarushTelegramBot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class JavarushTelegrambotApplicationTests {
+
+    @MockitoBean
+    private JavarushTelegramBot javarush_RX_community_bot;
 
 	@Test
 	void contextLoads() {
