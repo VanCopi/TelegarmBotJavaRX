ccba1231b02ad01a8d09ebf22f9f1cdf3506a13c
Van <sidorenkov-ia@yandex.ru>
2026-08-13 23:49:37 +0300

    * Solved problem with mock and tests

--- src/main/java/com/github/javarushcommunity/javarush_telegrambot/config/BotConfig.java
+++ src/main/java/com/github/javarushcommunity/javarush_telegrambot/config/BotConfig.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class BotConfig {
 
     @Bean
+    @Profile("!test")
     public TelegramBotsApi telegramBotsApi(JavarushTelegramBot bot) throws TelegramApiException {
         TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
         api.registerBot(bot);
--- src/test/java/com/github/javarushcommunity/javarush_telegrambot/JavarushTelegrambotApplicationTests.java
+++ src/test/java/com/github/javarushcommunity/javarush_telegrambot/JavarushTelegrambotApplicationTests.java
@@ -4,9 +4,23 @@ import com.github.javarushcommunity.javarush_telegrambot.bot.JavarushTelegramBot
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import static org.mockito.Mockito.mock;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class JavarushTelegrambotApplicationTests {
+
+    @Configuration
+    static class TestConfig {
+        @Bean
+        public TelegramBotsApi telegramBotsApi() {
+            return mock(TelegramBotsApi.class);
+        }
+    }
 
     @MockitoBean
     private JavarushTelegramBot javarush_RX_community_bot;
