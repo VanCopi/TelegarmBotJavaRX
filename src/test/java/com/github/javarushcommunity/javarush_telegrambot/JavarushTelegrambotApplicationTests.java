ec5e6c4002650250354efed46c62e6b2ff98df8f
Van <sidorenkov-ia@yandex.ru>
2026-08-14 00:14:35 +0300

    * Solved problem with mock and tests

--- src/test/java/com/github/javarushcommunity/javarush_telegrambot/JavarushTelegrambotApplicationTests.java
+++ src/test/java/com/github/javarushcommunity/javarush_telegrambot/JavarushTelegrambotApplicationTests.java
@@ -4,28 +4,11 @@ import com.github.javarushcommunity.javarush_telegrambot.bot.JavarushTelegramBot
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import static org.mockito.Mockito.mock;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-        "bot.username=test_bot",
-        "bot.token=test_token"
-})
 @ActiveProfiles("test")
 class JavarushTelegrambotApplicationTests {
-
-    @Configuration
-    static class TestConfig {
-        @Bean
-        public TelegramBotsApi telegramBotsApi() {
-            return mock(TelegramBotsApi.class);
-        }
-    }
 
     @MockitoBean
     private JavarushTelegramBot javarush_RX_community_bot;
